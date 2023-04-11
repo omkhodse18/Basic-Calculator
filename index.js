754373ca40e56b170ee33ef601527268804b6c82
Omkar Khodse <101259432+omkhodse18@users.noreply.github.com>
2023-04-11 19:59:12 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// let input = document.getElementById('display');
-// let buttons = document.querySelectorAll('button');
-// let arr = Array.from(button);
-// let string = '';
-
-// arr.forEach(buttons){
-
-// }
