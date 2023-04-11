08b83c144e859e0dcee23842c5482317a5ca2df0
omkhodse18 <omkarkhodse.18@gmail.com>
2023-04-11 19:56:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// let input = document.getElementById('display');
+// let buttons = document.querySelectorAll('button');
+// let arr = Array.from(button);
+// let string = '';
+
+// arr.forEach(buttons){
+
+// }
